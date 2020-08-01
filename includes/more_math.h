4137544b3f585b939a884c190ecd433213588f6a
Kevin Colour <kecolour@gmail.com>
2020-07-31 23:47:58 -0700

    Vector add and update test

--- includes/more_math.h
+++ includes/more_math.h
@@ -6,16 +6,17 @@
 /*   By: home <home@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/31 20:30:47 by home              #+#    #+#             */
-/*   Updated: 2020/07/31 21:19:01 by home             ###   ########.fr       */
+/*   Updated: 2020/07/31 23:45:48 by home             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MOORE_MATH_H
 # define MOORE_MATH_H
 
-#include <math.h>
+# include <math.h>
+# include <stdarg.h>
 
-#include "vector.h"
+# include "vector.h"
 
 double	vector_length(t_vector2f vector);
 void	fill_vector(t_vector2f *vector, double x, double y);
--- src/vector.c
+++ src/vector.c
@@ -6,7 +6,7 @@
 /*   By: home <home@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/31 21:07:53 by home              #+#    #+#             */
-/*   Updated: 2020/07/31 21:18:54 by home             ###   ########.fr       */
+/*   Updated: 2020/07/31 23:47:26 by home             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,10 @@ double	vector_length(t_vector2f vector)
 	result = (vector.x * vector.x) + (vector.y * vector.y);
 	result = sqrt(result);
 	return (result);
+}
+
+void	vector_add(t_vector2f *dest, t_vector2f a, t_vector2f b)
+{
+	dest->x = a.x + b.x;
+	dest->y = a.y + b.y;
 }
