dabf7275856cb57325b441e63c962c986623639b
Kevin Colour <kecolour@gmail.com>
2020-08-17 00:33:29 -0700

    function declaration

--- includes/more_math.h
+++ includes/more_math.h
@@ -6,7 +6,7 @@
 /*   By: home <home@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/31 20:30:47 by home              #+#    #+#             */
-/*   Updated: 2020/08/16 04:22:45 by home             ###   ########.fr       */
+/*   Updated: 2020/08/17 00:33:09 by home             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,13 @@
 # include "vector.h"
 # include "circle.h"
 
-double	vector_length(t_vector2f vector);
 void	fill_vector(t_vector2f *vector, double x, double y);
+
+double	vector_length(t_vector2f vector);
+
+void	vector_add(t_vector2f *dest, t_vector2f a, t_vector2f b);
+void	vector_scale(t_vector2f *dest, t_vector2f vec, double scale);
+void	vector_rescale(t_vector2f *dest, t_vector2f vec, double unit);
 
 void	normal_vector(t_vector2f *dest, t_vector2f axis, t_vector2f vec);
 void	tangent_vector(t_vector2f *dest, t_vector2f axis, t_vector2f vec);
